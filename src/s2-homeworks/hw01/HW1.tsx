85c4818d9eb74c979ae7791808059442b5b00fdb
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-02-01 21:51:18 +0100

    first commit

---
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import Message from './message/Message'
+import MessageSender from './message-sender/MessageSender'
+import s2 from '../../s1-main/App.module.css'
+import FriendMessage from './friend-message/FriendMessage'
+import avatar from './avatar.png'
+
+/*
+* 1 - описать тип MessageType
+* 2 - описать тип MessagePropsType в файле Message.tsx
+* 3 - в файле Message.tsx отобразить приходящие данные
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 5 - сделать стили в соответствии с дизайном
+* */
+
+// нужно создать правильный тип вместо any
+export type MessageType = {
+    id: number;
+    user: {
+        avatar: string;
+        name: string;
+    };
+    message: {
+        text: string;
+        time: string;
+    }
+}
+
+// структуру объекта не менять
+export const message0: MessageType = {
+    id: 0,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Alex',  // можно менять
+    },
+    message: {
+        text: 'Hello, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', // можно менять
+        time: '22:00', // можно менять
+    },
+}
+export const friendMessage0: MessageType = {
+    id: 100,
+    user: {
+        avatar: avatar, // можно менять
+        name: 'Ivan', // можно менять
+    },
+    message: {
+        text: 'How are you?', // можно менять
+        time: '22:00', // можно менять
+    },
+}
+
+const HW1 = () => {
+    return (
+        <div id={'hw1'}>
+            <div className={s2.hwTitle}>Homework #1</div>
+            <div className={s2.hw}>
+                {/*проверка отображения (не менять)*/}
+                <div>
+                    <Message message={message0} />
+                    <FriendMessage message={friendMessage0} />
+                </div>
+
+                {/*для автоматической проверки дз (не менять)*/}
+                <MessageSender M={Message} />
+            </div>
+        </div>
+    )
+}
+
+export default HW1
