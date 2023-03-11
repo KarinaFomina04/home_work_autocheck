13c87f00ecbf29c0d4742a4e37a4677ac41339ac
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-11 14:48:11 +0100

    Add HW6(local storage)

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,8 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+       setValue(restoreState('hw6-editable-span-value', value))
     }
 
     return (
