00c90ca9172ecaa262cbe95b77eb8bcd727ce947
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-18 13:05:54 +0100

    Add HW7

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,8 +5,7 @@ import HW2 from '../s2-homeworks/hw02/HW2'
 import HW3 from '../s2-homeworks/hw03/HW3'
 import HW4 from '../s2-homeworks/hw04/HW4'
 import HW5 from '../s2-homeworks/hw05/HW5'
-import HW6 from '../s2-homeworks/hw06/HW6'
-import HW7 from "../s2-homeworks/hw07/HW7";
+
 
 function App() {
     return (
@@ -19,8 +18,6 @@ function App() {
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
-            <HW6/>
-            <HW7/>
         </div>
     )
 }
