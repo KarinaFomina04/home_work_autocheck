de2e3f85764dd7b82c85ae4447b51da3c60fe133
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-26 20:35:05 +0200

    Add HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,30 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType):  UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let stateCope = [...state]
+            action.payload === "up"
+            ? stateCope.sort((a, b)=>{
+                if (a.name < b.name){return -1}
+                if (a.name > b.name){return 1}
+                return 0
+            })
+                : stateCope.sort((a, b)=>{
+                        if (a.name > b.name){return -1}
+                        if (a.name < b.name){return 1}
+                        return 0
+                    })
+            return stateCope
         }
         case 'check': {
-
+            state.filter(e => e.age >= action.payload)
             return state // need to fix
         }
         default:
             return state
     }
 }
+
+
